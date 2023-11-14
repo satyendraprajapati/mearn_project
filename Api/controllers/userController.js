4aa00b776b66684424c9c1d667be4fd33f59087b
Satyendra Prajapati <satya15793@gmail.com>
2023-11-14 00:20:47 -0800

    create sign in API route

--- Api/controllers/userController.js
+++ Api/controllers/userController.js
@@ -1,5 +1,7 @@
 import { User } from "../models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,6 +10,24 @@ export const signup = async (req, res, next) => {
     const newUser = new User({ username, email, password: hashPassword });
     await newUser.save();
     res.status(201).json({ message: "user created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "user not found"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(404, "wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {});
+
+    const { password: pass, ...userInfo } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(userInfo);
   } catch (error) {
     next(error);
   }
--- Api/routes/userRoute.js
+++ Api/routes/userRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { signup } from "../controllers/userController.js";
+import { signIn, signup } from "../controllers/userController.js";
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signIn);
 
 export default router;
